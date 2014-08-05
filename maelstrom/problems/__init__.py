60ff6d22c66621b43716789524f7d84618c59d12
Nico Schlömer <nico.schloemer@gmail.com>
2014-08-05 12:36:39 +0200

    fewer relative imports

--- maelstrom/problems/__init__.py
+++ maelstrom/problems/__init__.py
@@ -18,5 +18,5 @@
 #  You should have received a copy of the GNU General Public License
 #  along with Maelstrom.  If not, see <http://www.gnu.org/licenses/>.
 #
-from . import misc
-from . import crucible
+import misc
+import crucible
