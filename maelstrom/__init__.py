3391de43d811287993aacf42385b386c7af80850
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-22 14:09:12 +0100

    remove more GPL headers

--- maelstrom/__init__.py
+++ maelstrom/__init__.py
@@ -1,32 +1,14 @@
 # -*- coding: utf-8 -*-
 #
-#  Copyright (c) 2012--2014, Nico Schlömer, <nico.schloemer@gmail.com>
-#  All rights reserved.
+# import message
+# import materials_database
+# import maxwell_cylindrical
+# # import navier_stokes_cartesian
+# import heat_cylindrical
+# import navier_stokes_cylindrical
+# import time_steppers
 #
-#  This file is part of Maelstrom.
-#
-#  Maelstrom is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Maelstrom is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with Maelstrom.  If not, see <http://www.gnu.org/licenses/>.
-#
-#import message
-#import materials_database
-#import maxwell_cylindrical
-## import navier_stokes_cartesian
-#import heat_cylindrical
-#import navier_stokes_cylindrical
-#import time_steppers
-#
-#import problems
+# import problems
 __all__ = [
   'message',
   'materials_database',
