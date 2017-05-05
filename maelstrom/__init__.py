01f58a3dfcadd85152b585622f125f3324f1cad2
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-05 12:50:03 +0200

    fix for __init__

--- maelstrom/__init__.py
+++ maelstrom/__init__.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-import message
+from . import message
 # import maxwell
 # import heat
 # import navier_stokes
 # import time_steppers
 #
-# import problems
-__all__ = [
-  'message',
-  'maxwell',
-  'heat',
-  'navier_stokes',
-  'time_steppers'
-  ]
-
 # import problems
