b0d3d4b3b8a4e9dc9eae2cceec9adf32b22ad298
Nico Schlömer <nico.schloemer@gmail.com>
2017-06-14 17:42:34 +0200

    heat example test

--- examples/test_heat.py
+++ examples/test_heat.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*-
 #
 from dolfin import (
-    plot, interactive, Expression, FacetNormal, triangle, dot, grad, pi, dx,
-    ds, Constant, Measure, XDMFFile, solve, Function, DirichletBC,
-    TrialFunction, TestFunction, lhs, rhs, SpatialCoordinate
+    plot, interactive, FacetNormal, dot, grad, pi, dx, ds, Constant, Measure,
+    XDMFFile, solve, Function, DirichletBC, TrialFunction, TestFunction, lhs,
+    rhs, SpatialCoordinate
     )
 
-from maelstrom.materials_database import MATERIAL_PARAMETERS as params
+import problems
+
 import maelstrom.time_steppers as ts
-# import maelstrom.stabilization as stab
-import maelstrom.problems as probs
+
+import pytest
 
 
 def _main():
@@ -21,9 +22,9 @@ def _main():
     # T = {'upper': 1480.0,
     #      'upper left': 1500.0,
     #      'crucible': 1660.0}
-    _solve(
+    test_solve(
         # heat_transfer_coefficient, T,
-        stationary=True
+        stationary=False
         )
     return
 
@@ -60,7 +61,7 @@ def _parameter_quest():
         flux = {'upper': p[0],
                 'crucible': p[1]}
 
-        theta = _solve(flux, stationary=True)
+        theta = test_solve(flux, stationary=True)
 
         # Check if temperature values match with crucible blueprint.
         dev = np.array([theta(c[0]) - c[1] for c in control_values])
@@ -114,40 +115,31 @@ def _get_weak_F(mesh, f, kappa, rho_cp, b,
     return weak_F
 
 
-def _solve(stationary):
+@pytest.mark.parametrize('stationary', [
+    True, False
+    ])
+def test_solve(stationary):
 
-    # mesh,f,boundaries, boundary_indices, theta0 = probs.coil_in_box()
+    problem = problems.Crucible()
+
     #
-    # mesh,f,boundaries, boundary_indices, theta0 = \
-    #     probs.square_with_obstacle()
-    #
-    # mesh, subdomains, coils, wpi, subdomain_materials, omega, \
-    # (wp_boundaries, W, u_bcs, p_bcs, \
-    # Q, theta_bcs_d, theta_bcs_d_strict, theta_bcs_n \
-    #   = probs.crucible()
-    #
-    mesh, subdomains, subdomain_materials, workpiece_index, \
-        wp_boundaries, W, u_bcs, p_bcs, \
-        Q, theta_bcs_d, theta_bcs_n, theta_bcs_r \
-        = probs.peter()
-    #
-    mesh = W.mesh()
-    boundaries = wp_boundaries
+    mesh = problem.W.mesh()
+    boundaries = problem.wp_boundaries
 
     background_temp = 1500.0
     theta0 = Constant(background_temp)
 
     f = Constant(0.0)
 
-    material = subdomain_materials[workpiece_index]
-    rho = params[material]['density'](background_temp)
-    cp = params[material]['specific heat capacity'](background_temp)
-    kappa = params[material]['thermal conductivity'](background_temp)
+    material = problem.subdomain_materials[problem.wpi]
+    rho = material.density(background_temp)
+    cp = material.specific_heat_capacity
+    kappa = material.thermal_conductivity
 
     dt = 1.0e1
     end_time = 1000.0
 
-    ds = Measure('ds')[boundaries]
+    ds = Measure('ds')(subdomain_data=boundaries)
 
     b = Constant((0.0, 0.0))
 
@@ -159,7 +151,6 @@ def _solve(stationary):
     # The corresponding operator in weak form.
     rho_cp = rho*cp
     r = SpatialCoordinate(mesh)[0]
-    n = FacetNormal(mesh)
 
     def weak_F(t, u_t, u, v):
         f.t = t
@@ -167,31 +158,28 @@ def _solve(stationary):
             - dot(b, grad(u)) * v * 2*pi*r*dx \
             + f * v/rho_cp * 2*pi*r*dx
         # Neumann boundary conditions
-        for k, gradT in theta_bcs_n.items():
-            F += r * kappa * dot(n, gradT) * v/rho_cp * 2*pi*ds(k)
+        for k, n_grad_T in problem.theta_bcs_n.items():
+            F += r * kappa * n_grad_T * v/rho_cp * 2*pi*ds(k)
         # Robin boundary conditions
-        for k, value in theta_bcs_r.items():
+        for k, value in problem.theta_bcs_r.items():
             alpha, u0 = value
             F -= r * kappa * alpha * (u - u0) * v/rho_cp * 2*pi*ds(k)
         return F
 
-    theta_file = XDMFFile('results/temperature.xdmf')
-    theta_file.parameters['flush_output'] = True
-    theta_file.parameters['rewrite_function_mesh'] = False
     if stationary:
         t = 0.0
         # u_t = Constant(0.0)
-        u = TrialFunction(Q)
-        v = TestFunction(Q)
+        u = TrialFunction(problem.Q)
+        v = TestFunction(problem.Q)
         steady_F = weak_F(None, None, u, v)
         a = lhs(steady_F)
         L = rhs(steady_F)
         # Solve the stationary problem with the strict Dirichlet boundary
         # conditions, and extract dtheta/dn from the solution.
-        theta_reference = Function(Q, name='temperature')
+        theta_reference = Function(problem.Q, name='temperature')
         solve(a == L,
               theta_reference,
-              bcs=theta_bcs_d,
+              bcs=problem.theta_bcs_d,
               # bcs=theta_bcs_d_strict,
               solver_parameters={
                   'linear_solver': 'iterative',
@@ -204,18 +192,23 @@ def _solve(stationary):
                                     'monitor_convergence': False}
                   })
 
-        theta_file << theta_reference
+        with XDMFFile('temperature.xdmf') as f:
+            f.parameters['flush_output'] = True
+            f.parameters['rewrite_function_mesh'] = False
+            f.write(theta_reference)
+
         plot(theta_reference, rescale=True)
         interactive()
         exit()
         # Create new Dirichlet boundary conditions with the reference
         # solution.
         theta_bcs_d_new = []
-        for d in theta_bcs_d:
-            theta_bcs_d_new.append(DirichletBC(d.function_space(),
-                                               theta_reference,
-                                               d.domain_args[0]))
-        theta = Function(Q)
+        for d in problem.theta_bcs_d:
+            V = d.function_space()
+            theta_bcs_d_new.append(
+                DirichletBC(V, theta_reference, d.domain_args[0])
+                )
+        theta = Function(problem.Q)
         solve(a == L,
               theta,
               bcs=theta_bcs_d_new,
@@ -228,33 +221,42 @@ def _solve(stationary):
                                     'maximum_iterations': 100,
                                     'monitor_convergence': False}
                   })
-        theta_file << theta_reference
+
+        with XDMFFile('temperature.xdmf') as f:
+            f.parameters['flush_output'] = True
+            f.parameters['rewrite_function_mesh'] = False
+            f.write(theta_reference)
+
         plot(theta_reference)
         interactive()
         return theta
     else:
-        theta_1 = Function(Q)
+        theta_1 = Function(problem.Q)
         theta_1.interpolate(theta0)
         t = 0.0
-        while t < end_time:
-            theta = ts.implicit_euler_step(
-                    Q,
-                    weak_F,
-                    theta_1,
-                    t, dt,
-                    dx=2*pi*r*dx,
-                    sympy_dirichlet_bcs=theta_bcs_d,
-                    tol=1.0e-10,
-                    verbose=False,
-                    # problem is symmetric =>
-                    krylov='cg',
-                    preconditioner='hypre_amg'
-                    )
-            theta_1.assign(theta)
-            theta_file << (theta_1, t)
-            plot(theta_1, title='temperature')
-            # interactive()
-            t += dt
+        with XDMFFile('results/temperature.xdmf') as f:
+            f.parameters['flush_output'] = True
+            f.parameters['rewrite_function_mesh'] = False
+
+            while t < end_time:
+                theta = ts.implicit_euler_step(
+                        problem.Q,
+                        weak_F,
+                        theta_1,
+                        t, dt,
+                        dx=2*pi*r*dx,
+                        sympy_dirichlet_bcs=problem.theta_bcs_d,
+                        tol=1.0e-10,
+                        verbose=False,
+                        # problem is symmetric =>
+                        krylov='cg',
+                        preconditioner='hypre_amg'
+                        )
+                theta_1.assign(theta)
+                f.write(theta_1, t)
+                plot(theta_1, title='temperature')
+                # interactive()
+                t += dt
     return
 
 
