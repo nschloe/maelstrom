730cc4e8a9efa98cf33b33af18440ad471614078
Nico Schlömer <nico.schloemer@gmail.com>
2017-06-16 16:15:39 +0200

    clean up heat test

--- examples/test_heat.py
+++ examples/test_heat.py
@@ -4,14 +4,12 @@
 from __future__ import print_function
 
 from dolfin import (
-    plot, interactive, dot, grad, pi, dx, Constant, Measure, Function,
-    SpatialCoordinate, project, XDMFFile
+    plot, interactive, dx, Constant, Measure, Function, project, XDMFFile
     )
 
 import problems
 
 import maelstrom
-# import maelstrom.time_steppers as ts
 
 import parabolic
 
@@ -124,11 +122,9 @@ def test_stationary_solve(show=False):
     return
 
 
-def test_time_step(show=False):
+def test_time_step():
     problem = problems.Crucible()
 
-    #
-    mesh = problem.W.mesh()
     boundaries = problem.wp_boundaries
 
     background_temp = 1500.0
@@ -141,32 +137,6 @@ def test_time_step(show=False):
     kappa = material.thermal_conductivity
 
     my_ds = Measure('ds')(subdomain_data=boundaries)
-
-    b = Constant((0.0, 0.0))
-
-    # b_tau = stab.supg2(Q.mesh(),
-    #                    b,
-    #                    kappa/rho_cp,
-    #                    Q.ufl_element().degree()
-    #                    )
-    # The corresponding operator in weak form.
-    rho_cp = rho*cp
-    r = SpatialCoordinate(mesh)[0]
-
-    # pylint: disable=unused-argument
-    def weak_F(t, u_t, u, v):
-        f.t = t
-        F = - r * kappa * dot(grad(u), grad(v/rho_cp)) * 2*pi*dx \
-            - dot(b, grad(u)) * v * 2*pi*r*dx \
-            + f * v/rho_cp * 2*pi*r*dx
-        # Neumann boundary conditions
-        for k, n_grad_T in problem.theta_bcs_n.items():
-            F += r * kappa * n_grad_T * v/rho_cp * 2*pi*my_ds(k)
-        # Robin boundary conditions
-        for k, value in problem.theta_bcs_r.items():
-            alpha, u0 = value
-            F -= r * kappa * alpha * (u - u0) * v/rho_cp * 2*pi*my_ds(k)
-        return F
 
     # from dolfin import DirichletBC
     convection = None
@@ -191,10 +161,7 @@ def test_time_step(show=False):
     theta0.rename('theta0', 'temperature')
 
     theta1 = Function(problem.Q)
-    # theta1 = Function(problem.Q)
-
-    # plot(theta0)
-    # interactive()
+    theta1 = Function(problem.Q)
 
     t = 0.0
     dt = 1.0e-3
@@ -210,10 +177,11 @@ def test_time_step(show=False):
             theta0.assign(theta1)
             t += dt
             #
-            # plot(theta0)
-            # interactive()
-            #
             f.write(theta0, t)
+
+    assert abs(
+        maelstrom.helpers.average(theta0) - 1499.9998244919047
+        ) < 1.0e-3
 
     return
 
