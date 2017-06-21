0711fc1963fdd5ed453d76eacfe5d3e507682a9b
Nico Schlömer <nico.schloemer@gmail.com>
2017-06-21 09:52:41 +0200

    fix for documentation building

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ for mod_name in MOCK_MODULES:
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -32,11 +32,12 @@ for mod_name in MOCK_MODULES:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.mathjax',
-              'sphinxcontrib.bibtex',
-              'sphinx.ext.todo'
-              ]
+extensions = [
+        'sphinx.ext.autodoc',
+        'sphinx.ext.mathjax',
+        'sphinxcontrib.bibtex',
+        'sphinx.ext.todo'
+        ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
